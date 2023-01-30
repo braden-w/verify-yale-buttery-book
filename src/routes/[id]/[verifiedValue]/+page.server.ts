2217d69f8d927ef7f5ea067ed6dc1a73588c00bd
Braden Wong <bmw02002turbo@gmail.com>
2023-01-30 01:05:51 -0500

    feat: no redir

--- src/routes/[id]/[verifiedValue]/+page.server.ts
+++ src/routes/[id]/[verifiedValue]/+page.server.ts
@@ -1,7 +1,6 @@
 import type { PageServerLoad } from './$types';
 import { setButteryVerified } from './setButteryVerified';
 
-export const load = (async ({ params: { id, verifiedValue } }) => {
+export const load = (async ({ params: { id, verifiedValue }, url }) => {
 	await setButteryVerified(id, verifiedValue as 'OPEN' | 'CLOSED');
-	// throw error(404, 'Not found');
 }) satisfies PageServerLoad;
